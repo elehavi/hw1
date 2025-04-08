dc486b17fb4dcc7866e6c0be27af380385ac4955
Ella Lehavi <elehavi@ucsc.edu>
2025-04-08 16:25:38 -0700

    majority circuit tests

--- src/test/scala/hw1/MajorityCircuitTester.scala
+++ src/test/scala/hw1/MajorityCircuitTester.scala
@@ -8,8 +8,16 @@ class MajorityCircuitTester extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "MajorityCircuit"
   it should "correctly pass tests on all 8 possible inputs" in {
     test(new MajorityCircuit) { dut =>
-      // FILL IN HERE
-			???
+      for(a <- Seq(true, false)) {
+        for(b <- Seq(true, false)) {
+          for(c <- Seq(true, false)){
+            dut.io.a.poke(a.B)
+            dut.io.b.poke(b.B)
+            dut.io.c.poke(c.B)
+            dut.io.out.expect(((a & b) | (a & c) | (b & c)).B)
+          }
+        }
+      }
     }
   }
 }
